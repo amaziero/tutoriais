7056c37b3ca8ead74d7aaa70cde427f9eaa689b8
amaziero <alison_lens@hotmail.com>
2020-12-05 09:38:12 -0300

    tests and documentation

--- elixir/faturas/lib/faturas.ex
+++ elixir/faturas/lib/faturas.ex
@@ -1,9 +1,17 @@
-defmodule Fatura do
-  def criar_fatura do
-    ["Telefone", "Lux", "Agua"]
+defmodule Faturas do
+@moduledoc """
+  Este módulo executamos funções de faturas
+"""
+@doc """
+  Ao receber `fatura`, retorna um array de fatura
+    ## Exexmplos
+    iex> Faturas.criar_fatura(["Telefone", "Agua", "Luz"])
+"""
+  def criar_fatura(fatura) do
+    fatura
   end
 
-  def ordenar_faturas(fatura) do
+  def ordenar_fatura(fatura) do
     Enum.sort(fatura)
   end
 end
--- elixir/faturas/test/faturas_test.exs
+++ elixir/faturas/test/faturas_test.exs
@@ -1,8 +1,14 @@
-defmodule FaturasTest do
+defmodule FaturaTest do
   use ExUnit.Case
   doctest Faturas
 
-  test "greets the world" do
-    assert Faturas.hello() == :world
+  test "deve criar uma lista de faturas" do
+    faturas = Faturas.criar_fatura(["Agua", "Telefone", "Luz"])
+    assert faturas == ["Agua", "Telefone", "Luz"]
+  end
+
+  test "deve devolver uma lista ordenada de faturas" do
+    faturas = Faturas.criar_fatura(["Agua", "Telefone", "Luz"])
+    refute Faturas.ordenar_fatura(faturas) == ["Agua", "Telefone", "Luz"]
   end
 end
