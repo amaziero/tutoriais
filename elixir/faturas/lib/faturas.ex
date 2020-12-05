ac49413ce1a7971247c1211d10ab93a2461a7a89
amaziero <alison_lens@hotmail.com>
2020-12-05 12:11:47 -0300

    for com elixir

--- elixir/faturas/lib/faturas.ex
+++ elixir/faturas/lib/faturas.ex
@@ -5,13 +5,34 @@ defmodule Faturas do
 @doc """
   Ao receber `fatura`, retorna um array de fatura
     ## Exexmplos
-    iex> Faturas.criar_fatura(["Telefone", "Agua", "Luz"])
+    iex> Faturas.criar_faturas(["Telefone", "Agua", "Luz"])
 """
-  def criar_fatura(fatura) do
-    fatura
+  def criar_faturas(faturas, _vencimentos) do
+    for fatura <- faturas do
+      fatura
+    end
   end
+
+@doc """
+  Ao receber `fatura`, retorna um array de fatura
+    ## Exexmplos
+    iex> Faturas.ordenar_fatura(Faturas.criar_fatura(["Telefone", "Agua", "Luz"]))
+    ["Agua", "Luz", "Telefone"]
+"""
 
   def ordenar_fatura(fatura) do
     Enum.sort(fatura)
+  end
+
+@doc """
+  Ao receber um objeto `faturas` e verifica se o elemento `fatura`, existe no
+  objeto fatura
+    ## Exexmplos
+    iex> Faturas.fatura_existe?(Faturas.criar_fatura(["Telefone", "Agua", "Luz"]), "Luz")
+    true
+  """
+
+  def fatura_existe?(faturas, fatura) do
+    Enum.member?(faturas, fatura)
   end
 end
--- elixir/faturas/test/faturas_test.exs
+++ elixir/faturas/test/faturas_test.exs
@@ -11,4 +11,8 @@ defmodule FaturaTest do
     faturas = Faturas.criar_fatura(["Agua", "Telefone", "Luz"])
     refute Faturas.ordenar_fatura(faturas) == ["Agua", "Telefone", "Luz"]
   end
+
+  test "deve verificar se a conta existe" do
+    assert Faturas.fatura_existe?(Faturas.criar_fatura(["Telefone", "Agua", "Luz"]), "Luz")
+  end
 end
